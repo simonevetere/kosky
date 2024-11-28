f9361b4a78c266a614449ad656c6ba76d82ae4e8
Simone Vetere <simoneveterex@gmail.com>
2024-11-28 16:51:34 +0100

    fix vari e minify

---
+++ amministrazione/script.js
@@ -0,0 +1,33 @@
+// Controlla se l'email è presente nel localStorage
+const email = localStorage.getItem('email');
+
+if (email) {
+  // Email presente, crea i link
+  document.body.innerHTML = `
+  
+      <div id="back-dot" onclick="window.location.href='/?version=1'">
+        <img src="/img/back.png" alt="Back" />
+      </div>
+      
+      <dotlottie-player onclick="window.location = 'https://terribile.space'" src="https://lottie.host/9ee46b02-06d9-4f3d-b58d-894706d76640/Ug2gQBGp1U.json" background="transparent" speed="1" style="position: fixed;z-index: 9999;width: 64px; height: 64px; left: 0; top: 0;" loop autoplay></dotlottie-player>
+
+      <div id="inside">
+        <a href="https://webmail.register.it/appsuite">Mail</a>
+        <a href="#">YouTube (Work in progress)</a>
+        <a href="mailto:${email}">Contattami (${email})</a>
+      </div>
+
+      <footer>
+        <br> &copy; 2024 La Taverna del Kosky <br><br>Code by &copy;2024 <a href="https://terribile.space">terribile.SPACE</a> 
+      </footer>
+  `;
+
+  // Aggiungi lo script dotlottie dinamicamente
+  const script = document.createElement('script');
+  script.src = "https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs";
+  script.type = "module";
+  document.head.appendChild(script); 
+} else {
+  // Email non presente, reindirizza a /login
+  window.location = '/login'; 
+}
